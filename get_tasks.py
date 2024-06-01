8a570addce5368549cae71c811b6950103c140b3
shenli <15200831505@163.com>
2024-06-01 23:19:12 +0800

    获取新的任务，增加条件

--- get_tasks.py
+++ get_tasks.py
@@ -12,11 +12,13 @@
 -------------------------------------------------
 """
 import time
+from datetime import datetime
 from common import post_request, get_new_tasks, post_headers
 
 
 if __name__ == "__main__":
-    token = "eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6IjA1NjlhMWQzLWE1ODgtNDFmYS04ODg3LTJkNmZlNTc0ZDU4NiJ9.sLG4CC9j7GlzfdmXD55luKY6qfC8aQiJ6RvnXMPAQ2hJc-vB-F8UduYanUznHnis6eeoiMQvqw9FQ9OyKOr8xw"           # 填充 token
+    # TODO 填充 token
+    token = "eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6IjZiN2JjNDdlLTJjNDItNGY2NC1iOGE2LWYzYTJjMTNjMzdiOSJ9.nuC0QNkOlzOv_h0viBzhgnTPFZEO_4r0A0P0054vToppUCyKxGjjMU6U_xhMNaNrxZzBen6s8sc_lUfMDJU7sw"
     post_headers(token)  # headers 加入 token
 
     tasks_count = 20
@@ -25,11 +27,20 @@ if __name__ == "__main__":
     }       # 每次领取 20 道题
 
     while True:
-        results = post_request(get_new_tasks, **post_data)
-        if results["stateCode"] == "200":       # 成功获取
-            print(f"已获取 {tasks_count} 道题")
-            break
+        formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")       # 获取当前时间戳
 
-        print(f"休息一会，重新常识获取 {tasks_count} 道题")
-        time.sleep(5)
+        results = post_request(get_new_tasks, **post_data)
+        try:
+            if results["msg"] == "存在未完成的任务，不能获取视频任务":
+                print(f"{formatted_time}\t\t存在未完成的任务，不能获取视频任务")
+                break
+
+            if results["stateCode"] == "200":  # 成功获取
+                print(f"{formatted_time}\t\t已获取{tasks_count}道题")
+                break
+
+        except KeyError:        # results["code"] = 500     results["msg"] = "当前视频池没有数据"
+            print(f"{formatted_time}\t\t休息一会，重新尝试获取{tasks_count}道题")
+
+        time.sleep(1)
 
